506e6a70cea5059528b0417c2299db3a400c665c
aryanm5 <aryanm.email@gmail.com>
2023-11-10 23:26:14 -0800

    added logo and styling

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -2,6 +2,7 @@ import { useState, useEffect } from "react";
 import "./App.css";
 import axios from 'axios';
 import Quote from './Quote';
+import logo from './logo.png';
 
 function App() {
     const [name, setName] = useState("");
@@ -39,36 +40,42 @@ function App() {
 
     return (
         <div className="App">
-            {/* TODO: include an icon for the quote book */}
-            <h1>Hack @ UCI Tech Deliverable</h1>
+            <div className="Container">
+                <img src={logo} className="logo" />
+                <h1>Hack @ UCI Tech Deliverable</h1>
 
-            <h2>Submit a quote</h2>
-            <form>
-                <label htmlFor="input-name">Name</label>
-                <input type="text" name="name" id="input-name" value={name} onChange={e => setName(e.target.value)} required />
-                <label htmlFor="input-message">Quote</label>
-                <input type="text" name="message" id="input-message" value={message} onChange={e => setMessage(e.target.value)} required />
-                <button type="submit" onClick={submitQuote} disabled={name.length === 0 || message.length === 0}>Submit</button>
-            </form>
+                <h3>Submit a Quote</h3>
+                <form>
+                    <label htmlFor="input-name">Name: </label>
+                    <input type="text" name="name" id="input-name" value={name} onChange={e => setName(e.target.value)} required />
+                    <br /><br />
+                    <label htmlFor="input-message">Quote: </label>
+                    <input type="text" name="message" id="input-message" value={message} onChange={e => setMessage(e.target.value)} required />
+                    <br /><br />
+                    <button type="submit" onClick={submitQuote} disabled={name.length === 0 || message.length === 0}>Submit</button>
+                </form>
 
-            <h2>Previous Quotes</h2>
+                <h3>Previous Quotes</h3>
 
-            <select
-                defaultValue={-1}
-                onChange={e => loadQuotes(e.target.value)}
-            >
-                <option value={604800}>Past Week</option>
-                <option value={2630000}>Past Month</option>
-                <option value={31536000}>Past Year</option>
-                <option value={-1}>Show All</option>
-            </select>
+                <select
+                    defaultValue={-1}
+                    onChange={e => loadQuotes(e.target.value)}
+                >
+                    <option value={604800}>Past Week</option>
+                    <option value={2630000}>Past Month</option>
+                    <option value={31536000}>Past Year</option>
+                    <option value={-1}>Show All</option>
+                </select>
 
-            <div className="messages">
-                {
-                    loading
-                        ? <p>Loading...</p>
-                        : quotes.map(quote => <Quote key={quote.time} quote={quote} />)
-                }
+                <div className="messages">
+                    {
+                        loading
+                            ? <p>Loading...</p>
+                            : quotes.length === 0
+                                ? <p>No Results</p>
+                                : quotes.map(quote => <Quote key={quote.time} quote={quote} />)
+                    }
+                </div>
             </div>
         </div>
     );
--- frontend/src/Quote.jsx
+++ frontend/src/Quote.jsx
@@ -1,6 +1,28 @@
+const dateOptions = {
+    year: 'numeric',
+    month: 'short',
+    day: 'numeric',
+};
+
 function Quote({ quote: { name, message, time } }) {
+    const date = new Date(time);
+
     return (
-        <p>{time} - {name}: {message}</p>
+        <div className="QuoteContainer">
+            <div>
+                <span className="QuoteName">
+                    <strong>{name}</strong>
+                </span>
+
+                <span className="QuoteDate">
+                    {date.toLocaleDateString('en-US', dateOptions)}
+                </span>
+                <br />
+            </div>
+            <div className="QuoteMessage">
+                {message}
+            </div>
+        </div>
     );
 }
 
